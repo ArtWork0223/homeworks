94d501d55eccbf0c469627f8a0e1b9b33f1b0ac5
ArtWork0223 <erick29122012@gmail.com>
2016-12-02 22:40:04 +0200

    first commit

---
+++ home-work1/js/main.js
@@ -0,0 +1,32 @@
+console.log('User Card')
+let name = "Artem";
+let surname = "Hevorkyan";
+let date_of_birth = "23.02.1992";
+let skills = "begginer";
+
+console.log(name);
+console.log(surname);
+console.log(date_of_birth);
+console.log(skills);
+
+console.log('')
+
+console.log('Friend User Card')
+let friendName = "Ivan";
+let friendSurName  = "Ivanov";
+let friendDate_of_birth = "08.11.1987";
+let friendSkills = "Professional";
+
+console.log(friendName);
+console.log(friendSurName);
+console.log(friendDate_of_birth);
+console.log(friendSkills);
+
+
+let banan = {};
+banan.color = "yellow";
+banan.count = 4;
+banan.price = [25,27];
+banan.ingr = {}
+banan.ingr.xyz = 777;
+console.log(banan);
